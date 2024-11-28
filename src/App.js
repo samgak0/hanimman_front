6b3b09231ffd3a872b96834adc8630a00ff4d7ba
p614080 <p614080@naver.com>
2024-11-28 14:49:23 +0900

    FAQ 연결

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Events from "./pages/Home/mainjs/Events";
 import Announcement from "./pages/Home/mainjs/Announcement";
 import AnnouncementDetail from "./pages/Home/mainjs/AnnouncementDetail";
 import FAQ from "./pages/Home/mainjs/FAQ";
+import FAQDetail from "./pages/Home/mainjs/FAQDetail";
 import Terms from "./pages/Home/mainjs/Terms";
 import authRoutes from "./routes/authRoutes";
 import mainRoutes from "./routes/mainRoutes";
@@ -76,6 +77,10 @@ const App = () => {
             element={<PrivateRoute element={<AnnouncementDetail />} />}
           />
           <Route path="/faq" element={<PrivateRoute element={<FAQ />} />} />
+          <Route
+            path="/faq/:id"
+            element={<PrivateRoute element={<FAQDetail />} />}
+          />
           <Route path="/terms" element={<PrivateRoute element={<Terms />} />} />
 
           {/* Main 관련 경로 */}
